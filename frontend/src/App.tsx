51ae50656d2f9f747ed4cf5501e2f04773e7a465
Aseeeem-kc <ashimkc7297@gmail.com>
2025-11-20 13:09:36 +0545

    feat: static analytics page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import ResetPasswordPage from './pages/ResetPasswordPage';
 import OAuthSuccess from "./pages/OAuthSuccess";
 import TicketSolverPage from "./pages/core/TicketSolverPage";
+import AnalyticsPage from "./pages/core/AnalyticsPage";
+
 function App() {
   return (
     <Router>
@@ -22,6 +24,7 @@ function App() {
         <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
         <Route path="/oauth-success" element={<OAuthSuccess />} />
         <Route path="/tickets" element={<TicketSolverPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
       </Routes>
     </Router>
   );
